eb9ffd3efb952c7466db89a1ae4ec4fef7dd2785
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-03-24 16:40:10 +0100

    Added Required to Entity AppUser

--- WebShop/Models/Enteties/AppUser.cs
+++ WebShop/Models/Enteties/AppUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebShop.Models.Enteties
 {
     public class AppUser  : IdentityUser
     {
+        [Required]
         public string Name { get; set; }
         public DateTime RegisterDate { get; set; } = DateTime.Now;
         public ICollection<Order> Orders { get; set; } = new List<Order>();
