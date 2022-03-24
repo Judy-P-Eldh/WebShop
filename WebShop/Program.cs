7a315a11626a520f1ae16f618f2bf054f343bb50
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-03-24 10:13:25 +0100

    Changed Required Account in Program

--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 //builder.Services.AddScoped<ProductRepository>();
