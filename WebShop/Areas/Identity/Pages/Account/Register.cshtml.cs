c9c312162ba320f2e82e6d7849df5b9ec4f9e6ad
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-03-24 14:13:06 +0100

    Changed Register and Log in. Fields.

--- WebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,16 @@ namespace WebShop.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [DataType(DataType.DateTime)]
+            public DateTime RegisterDate { get; set; }   = DateTime.Now;
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [DataType(DataType.Text)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -79,6 +89,7 @@ namespace WebShop.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
