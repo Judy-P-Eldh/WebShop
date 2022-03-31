f4a4b2d9c6834c6be1daa6bf1e92f3006a0566a7
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-03-31 14:22:33 +0200

    Added Dtos

---
+++ Plant.Core/DTOs/EventDto.cs
@@ -0,0 +1,18 @@
+﻿using Plant.Core.Enteties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plant.Core.DTOs
+{
+    internal class EventDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Address Address { get; set; }
+        public ICollection<Offer> Offers { get; set; } = new List<Offer>();
+    }
+}
---
+++ Plant.Core/DTOs/OfferDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plant.Core.DTOs
+{
+    internal class OfferDto
+    {
+        public string Title { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        public DateTime EndDate => StartDate.AddDays(7);
+        public string Description { get; set; }
+
+        [Display(Name = "Discount (%)")]
+        public int Discount { get; set; }
+    }
+}
